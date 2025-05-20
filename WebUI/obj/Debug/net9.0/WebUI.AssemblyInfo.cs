db73e9c3f641ca17e182a21465039e3b009383fe
RE22GDV <gandzukdmitrij4@gmail.com>
2025-05-20 03:03:06 +0300

    Web_Patch2_20-05-2025

--- WebUI/obj/Debug/net9.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net9.0/WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1df4ed774ba9b24b5fc6baad601768ac77041a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
